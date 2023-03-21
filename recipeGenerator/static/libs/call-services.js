271da8d19521268232c5a903bbef2f7e36cb57d2
kuijunming93 <kuijunming93@gmail.com>
2023-03-22 00:45:50 +0800

    Fix for iphone loade

--- recipeGenerator/static/libs/call-services.js
+++ recipeGenerator/static/libs/call-services.js
@@ -4,15 +4,7 @@ $(document).ready(function(){
         $('#imgGenModal.modal').modal('hide');
         $('body').removeClass('modal-open');
         $('.modal-backdrop').remove();
-
-        document.querySelector(".insertLoader").innerHTML = `
-        <div class="loader" style="background-color:#FFE5B4;">
-          <div>
-            <img src="/static/assets/loadscreen.gif" class="img-fluid">
-            <h3 class="loader-text mt-3" style="color:black">Generating your recipe, please wait..
-            <div class="spinner-grow text-warning" role="status"></div></h3>
-          </div>
-        </div>`;
+        document.querySelector(".insertLoader").classList.remove('d-none');
     })
 
     $('.option-item').click(function () {
--- recipeGenerator/static/libs/loadScreen.js
+++ recipeGenerator/static/libs/loadScreen.js
@@ -1,17 +1,6 @@
 // SHOW LOAD CANVAS WHEN CLICKED
-function showLoader(){
-    document.querySelector(".insertLoader").innerHTML = `
-        <div class="loader" style="background-color:#FFE5B4;">
-          <div>
-            <img src="/static/assets/loadscreen.gif" class="img-fluid">
-            <h3 class="loader-text mt-3" style="color:black">Generating your recipe, please wait..
-            <div class="spinner-grow text-warning" role="status"></div></h3>
-          </div>
-        </div>`;
-  }
-
 $(document).ready(function(){
     $('.insertLoader-btn').click(function({
-        showLoader();
+        document.querySelector(".insertLoader").classList.remove('d-none');
     }))
 })
